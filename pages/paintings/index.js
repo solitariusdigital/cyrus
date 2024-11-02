688f9d707132037fd0d9d87a81e3be702605a9fd
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-11-02 13:27:17 +0330

    updates

--- pages/paintings/index.js
+++ pages/paintings/index.js
@@ -2,12 +2,29 @@ import { useState, useEffect, useContext, Fragment } from "react";
 import { StateContext } from "@/context/stateContext";
 import classes from "./paintings.module.scss";
 import Image from "next/legacy/image";
+import GallerySlider from "@/components/GallerySlider";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Paintings() {
+  const [displayGallerySlider, setDisplayGallerySlider] = useState(false);
+
+  const works = [
+    "https://eshareh.storage.iran.liara.space/cover/cov720908/img6055.jpg",
+    "https://eshareh.storage.iran.liara.space/cover/cov913559/img2034.jpg",
+    "https://eshareh.storage.iran.liara.space/cover/cov156042/img6851.jpg",
+    "https://eshareh.storage.iran.liara.space/cover/cov720908/img6055.jpg",
+  ];
+
+  const gallerySlider = () => {
+    setDisplayGallerySlider(true);
+    window.scrollTo(0, 0);
+    document.body.style.overflow = "hidden";
+  };
+
   return (
     <Fragment>
       <div className={classes.container}>
-        <div className={classes.columnTwo}>
+        <div className={classes.columnTwo} onClick={() => gallerySlider()}>
           <div className={classes.imageBox}>
             <Image
               className={classes.image}
@@ -201,6 +218,20 @@ export default function Paintings() {
           </div>
         </div>
       </div>
+      {displayGallerySlider && (
+        <div className={classes.gallerySlider}>
+          <div className={classes.icon}>
+            <CloseIcon
+              onClick={() => {
+                setDisplayGallerySlider(false);
+                document.body.style.overflow = "auto";
+              }}
+            />
+          </div>
+          <h2>works</h2>
+          <GallerySlider media={works} />
+        </div>
+      )}
     </Fragment>
   );
 }
