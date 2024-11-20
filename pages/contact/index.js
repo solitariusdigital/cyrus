1cfdf452f5ff28e3cb1f8f20cfa490bfb937d5f5
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-11-20 17:28:59 +0330

    feat: contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,28 @@
-import React from "react";
+import classes from "./contact.module.scss";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.items}>
+        <div
+          className={classes.item}
+          onClick={() =>
+            window.open(
+              "https://www.instagram.com/panteacyrus.official/",
+              "_ self"
+            )
+          }
+        >
+          <InstagramIcon sx={{ fontSize: 30 }} />
+          <h3>panteacyrus.official</h3>
+        </div>
+        <div className={classes.item}>
+          <EmailIcon sx={{ fontSize: 30 }} />
+          <h3>cyruspantea@gmail.com</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
