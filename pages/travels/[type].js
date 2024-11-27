e67ff88165513bab854fbc4b1ee06707d5ff85c4
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-11-27 16:35:41 +0330

    updates

--- pages/travels/[type].js
+++ pages/travels/[type].js
@@ -15,14 +15,9 @@ export default function Type({ typeTitle }) {
   const [displayGallerySlider, setDisplayGallerySlider] = useState(false);
 
   const works = [
-    "https://cyrus.storage.c2.liara.space/photos/1bc62462-716f-469a-aebe-a91f2138e902.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/3a57e405-bec0-45df-8b6d-4ec0af2f64ea.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/6efa4ae2-fd3c-46d8-a92b-76d28f709948.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/8f11eb29-0da1-41da-8342-89f05eee3c3d.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/1bc62462-716f-469a-aebe-a91f2138e902.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/3a57e405-bec0-45df-8b6d-4ec0af2f64ea.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/6efa4ae2-fd3c-46d8-a92b-76d28f709948.JPG",
-    "https://cyrus.storage.c2.liara.space/photos/8f11eb29-0da1-41da-8342-89f05eee3c3d.JPG",
+    "https://cyrus.storage.c2.liara.space/assets/IMG_2852.JPG",
+    "https://cyrus.storage.c2.liara.space/assets/IMG_2854.JPG",
+    "https://cyrus.storage.c2.liara.space/assets/IMG_2851.JPG",
   ];
 
   useEffect(() => {
